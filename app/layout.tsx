e0a659ee3864649606f17c5a1bdd3038eb136db8
Eyob Tesfaye <eyobimpactinglife@gmail.com>
2025-08-07 21:54:31 +0300

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import "easymde/dist/easymde.min.css";
 
 
+
 const workSans = localFont({
   src: [
     {
--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -23,11 +23,12 @@ const StartupCard = ({post} : {post: StartupTypeCard}) => {
       </div>
       <div className='flex-between mt-5 gap-5'>
         <div className='flex-1' >
-          <Link href = {`/user/${post.author?._id}`}>
          <p className='text-16-medium line-clamp-1'>
+  <Link href={`/user/${post.author?._id}`}>
     {post?.author?.name ?? 'Unknown'}
-  </p>
-          </Link>
+  </Link>
+</p>
+
           <Link href = {`/startup/${post?._id}`}>
           <h3 className='text-26-semibold line-clamp-1' >
           {post?.title}
